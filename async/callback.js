675fa1bed2b6ea0f5fd811456f1768e5635ed363
mspcity <codepark2019@gmail.com>
2021-03-22 13:42:29 -0400

    link async/callback.js

---
+++ async/callback.js
@@ -0,0 +1,21 @@
+'use strict';
+// Synchronous callback
+function printImmediately(print) {
+  print();
+}
+// Asynchronous callback
+function printWithDelay(print, timeout) {
+  setTimeout(print, timeout);
+}
+
+// JavaScript is synchronous.
+// Execute the code block by order after hoisting.
+// hoisting: var, function declaration 
+console.log('1') // sync
+setTimeout(() => 
+console.log('2'), 1000); // async
+console.log('3') // sync
+
+printImmediately(() => console.log('hello')) // sync
+
+printWithDelay(() => console.log('async callback'), 2000) // async
