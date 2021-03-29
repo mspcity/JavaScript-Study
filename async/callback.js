8b0fd2684ba31148c18d74d6378e026c74aff372
mspcity <codepark2019@gmail.com>
2021-03-28 22:41:56 -0400

    code completed

--- async/callback.js
+++ async/callback.js
@@ -20,12 +20,47 @@ printImmediately(() => console.log('hello')) // sync
 
 printWithDelay(() => console.log('async callback'), 2000) // async
 
-// Call back Hell example
+// Callback Hell example
 class UserStorage {
   loginUser(id, password, onSuccess, onError) {
-
+      setTimeout(() => {
+        if (
+          (id === 'park' && password === 'dream') ||
+          (id === 'coder' && password === 'academy')
+        ) {
+          onSuccess(id);
+        } else {
+          onError(new Error('not found'));
+        }
+      }, 2000)
   }
   getRoles(user, onSuccess, onError) {
-    
+    setTimeout(() => {
+      if (user === 'park') {
+        onSuccess({ name: 'park', role: 'admin'});
+      } else {
+        onError(new Error('no access'));
+      }
+    }, 1000)
   }
 }
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(
+  id, 
+  password, 
+  user => {
+    userStorage.getRoles(
+      user, 
+      userWithRole => {
+        alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`)
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }, 
+error => {console.log(error)}
+);
