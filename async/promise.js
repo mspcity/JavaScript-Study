68a99c9a3d495ca649af39852be7b3fe164f6306
mspcity <codepark2019@gmail.com>
2021-04-04 20:33:15 -0400

    const promise = new Promise()

---
+++ async/promise.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Promise is a JavaScript object for asynchronous operations.
+// state: peding -> fulfilled or rejected
+// Producer vs Consumer
+
+// 1. Producer
+const promise = new Promise()
