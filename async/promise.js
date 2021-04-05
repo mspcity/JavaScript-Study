bf5631a1a1542b872b263676ed330c0258748ccb
mspcity <codepark2019@gmail.com>
2021-04-04 21:10:05 -0400

    promise.catch and promise.finally

--- async/promise.js
+++ async/promise.js
@@ -10,11 +10,19 @@ const promise = new Promise((resolve, reject) => {
   // doing some heavy work (network, read files)
   console.log('doing something..');
   setTimeout(() => {
-      resolve('park');
+      // resolve('park');
+      reject(new Error('no network'))
   }, 2000);
 });
 
 // 2. Consumers: then, catch, finally
-promise.then((value) => {
+promise
+.then((value) => {
   console.log(value);
-})Í
+})
+.catch(error => {
+  console.log(error)
+})
+.finally(() => {
+  console.log('finally')
+});
