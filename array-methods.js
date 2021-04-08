56c583a9edb4338fc723458d70db7eaea121321b
mspcity <codepark2019@gmail.com>
2021-04-08 04:31:39 -0400

    array-methods

---
+++ array-methods.js
@@ -0,0 +1,27 @@
+// let userList = [
+//   { name: "mike", age: 30 },
+//   { name: "jane", age: 27 },
+//   { name: "tom", age: 10 }
+// ];
+
+// const result = userList.find((user) => {
+//   if(user.age < 19) {
+//     return true;
+//   }
+//   return false;
+// })
+
+// console.log(result);
+
+// let arr = [1, 2, 3, 4, 5]
+// let result2 = arr.splice(1, 2);
+// console.log(arr)
+
+// arr.splice(1, 3, 100, 200)
+// console.log(arr)
+
+let arr = [1, 2, 3, 4, 5]
+
+console.log(arr.slice(1,4));
+
+let arr2 = [1, 2, 3, 4, 5]
