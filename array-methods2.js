825b7644be80c4cb1ab6463ea0de91565f736c14
mspcity <codepark2019@gmail.com>
2021-04-08 10:56:05 -0400

    indexOf

--- array-methods2.js
+++ array-methods2.js
@@ -1,6 +1,6 @@
 // 배열의 모든수 합치기
 
-let arr = [1,2,3,4,5]
+// let arr = [1,2,3,4,5]
 
 // for, for of, forEach
 
@@ -9,8 +9,16 @@ let arr = [1,2,3,4,5]
 //   result += num
 // })
 
-const result = arr.reduce((prev, cur) => {
-    return prev + cur
-}, 0)
+// const result = arr.reduce((prev, cur) => {
+//     return prev + cur
+// }, 0)
 
-console.log(result)
+// console.log(result)
+
+
+const arr = [1, 2, 4, 5, 6]
+ 
+ index = arr.indexOf(5) 
+ 
+ console.log(index)
+ 
