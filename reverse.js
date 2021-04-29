b5d6b5f846dd1617429f99c67e41563f78ea4f0e
mspcity <codepark2019@gmail.com>
2021-04-29 03:33:34 -0400

    practice

--- reverse.js
+++ reverse.js
@@ -1,9 +1,10 @@
-let str = "truck"
-let newStr = str.split('')
-console.log(newStr.reverse().join(''))
+// let str = "truck"
+// let newStr = str.split('')
+// console.log(newStr.reverse().join(''))
 
-// let newStr = ''
-// for(let i = str.length -1; i >= 0; i--) {
-//   newStr += str[i]
-// }
-// console.log(newStr)
+let str = 'apple'
+let newStr = ''
+for(let i = str.length -1; i >= 0; i--) {
+  newStr += str[i]
+}
+console.log(newStr)
